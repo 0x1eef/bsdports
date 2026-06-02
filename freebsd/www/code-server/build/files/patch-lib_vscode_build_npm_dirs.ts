9e9d84891077589c6d46a35add36ade2631c08d6
0x1eef <0x1eef@hardenedbsd.org>
2026-06-02 13:55:54 -0300

    www/code-server: banish copilot

---
+++ freebsd/www/code-server/build/files/patch-lib_vscode_build_npm_dirs.ts
@@ -0,0 +1,10 @@
+--- lib/vscode/build/npm/dirs.ts.orig
++++ lib/vscode/build/npm/dirs.ts
+@@ -15,7 +15,6 @@ export const dirs = [
+ 	'build/vite',
+ 	'extensions',
+ 	'extensions/configuration-editing',
+-	'extensions/copilot',
+ 	'extensions/css-language-features',
+ 	'extensions/css-language-features/server',
+ 	'extensions/debug-auto-launch',
