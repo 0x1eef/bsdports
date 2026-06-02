ce330f81630fa41d7cc3c56b3869383358e317d3
0x1eef <0x1eef@hardenedbsd.org>
2026-06-02 13:34:11 -0300

    www/code-server: workaround

--- freebsd/www/code-server/build/files/patch-lib_vscode_build_npm_postinstall.ts
+++ freebsd/www/code-server/build/files/patch-lib_vscode_build_npm_postinstall.ts
@@ -1,6 +1,6 @@
 --- lib/vscode/build/npm/postinstall.ts.orig
 +++ lib/vscode/build/npm/postinstall.ts
-@@ -56,10 +56,10 @@ async function npmInstallAsync(dir: string, opts?: child_proc
+@@ -57,10 +57,12 @@
  		env: { ...process.env },
  		...(opts ?? {}),
  		cwd: path.join(root, dir),
@@ -10,15 +10,17 @@
  
 -	const command = process.env['npm_command'] || 'install';
 +	const command = (process.env['npm_command'] || 'install').split(' ');
++	const npmCommand = process.platform === 'freebsd' && process.env['npm_execpath'] ? process.execPath : npm;
++	const npmArgs = process.platform === 'freebsd' && process.env['npm_execpath'] ? [process.env['npm_execpath'], ...command] : command;
  
  	if (process.env['VSCODE_REMOTE_DEPENDENCIES_CONTAINER_NAME'] && /^(.build\/distro\/npm\/)?remote$/.test(dir)) {
  		const syncOpts: child_process.SpawnSyncOptions = {
-@@ -88,7 +88,7 @@ async function npmInstallAsync(dir: string, opts?: child_proc
+@@ -88,7 +90,7 @@
  		run('sudo', ['chown', '-R', `${userinfo.uid}:${userinfo.gid}`, `${path.resolve(root, dir)}`], syncOpts);
  	} else {
  		log(dir, 'Installing dependencies...');
 -		const output = await spawnAsync(npm, command.split(' '), finalOpts);
-+		const output = await spawnAsync(npm, command, finalOpts);
++		const output = await spawnAsync(npmCommand, npmArgs, finalOpts);
  		if (output.trim()) {
  			for (const line of output.trim().split('\n')) {
  				log(dir, line);
